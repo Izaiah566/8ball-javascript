b6eefa6e86cfaf4788333ea4f0f34be9fd9d76cb
Izaiah <izaiahlharrison@gmail.com>
2025-05-19 17:25:04 -0400

    initial commit

---
+++ 8-ball/js/script.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const answers = [
+    "Yes",
+    "no",
+    "Maybe",
+    "Ask again later",
+    "Definitely",
+    "Absolutely",
+    "It is certain",
+    "Don't count on it",
+    "Very doubtful",
+    "Better not tell you now"];
+
+document.addEventListener("DOMContentLoaded", function () {
+    const question = document.querySelector("#question");
+    const answerDiv = document.querySelector("#answer");
+    const blackBall = document.querySelector(".black-ball")
+
+    question.addEventListener("click", function (){
+        const randomIndex = Math.floor(Math.random() * answers.length);
+        const answer = answers[randomIndex];
+        blackBall.className='black-ball animate'
+        if ((question.length === 0) && (question.charAt(question.length-1) !== '?'))
+        {
+            answerDiv.innerHTML = "Please ask a valid yes/no question.";
+            return;
+        }
+        setTimeout(()=>{
+            answerDiv.innerHTML = answer;
+            answerDiv.style.fontSize = '1.1rem'
+            blackBall.className='black-ball'
+        },3000);
+    });
+});
